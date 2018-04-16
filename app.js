d49c5e63590eea0eadad70842b6500093d51d17d
DESKTOP-LK4POK4\Judit <toth.jdt@gmail.com>
2018-04-16 12:47:00 +0200

    rpi

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const arpScanner = require('arpscan');
+
+arpScanner(onResult, options);
+
+function onResult(err, data){
+    if(err) throw err;
+    console.log(data);
+}
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
