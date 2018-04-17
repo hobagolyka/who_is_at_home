9cfc95725e777f0c48669bfeac84aa4e1bb56731
DESKTOP-LK4POK4\Judit <toth.jdt@gmail.com>
2018-04-17 12:47:15 +0200

    asdf

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const mysql = require('mysql');
-const hostname = '127.0.0.1';
-const port = 3000;
 const arpScanner = require('arpscan');
 
 // CONFIG
+const hostname = '127.0.0.1';
+const port = 3000;
+var db = [];
+var arp = [];
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : '',
@@ -12,8 +14,9 @@ var connection = mysql.createConnection({
     database : ''
 });
 
-function dbconnect(callback, insert) {
-    connection.query('SELECT * FROM macs',
+// FUNCTIONS
+function dbconnect(callback, query) {
+    connection.query(query,
         function(err,row){
         if (err) {
             throw err;
@@ -22,38 +25,26 @@ function dbconnect(callback, insert) {
     });
 }
 
+
 dbconnect(function(err, result){
     if (err) throw err;
     else {
         console.log(result);
+        db = result;
     }
     return;
-});
-
-var nmap = require('node-nmap');
-
-nmap.nmapLocation = "nmap"; //default
-
-//    Accepts array or comma separated string of NMAP acceptable hosts
-var quickscan = new nmap.QuickScan('127.0.0.1 google.com');
-
-quickscan.on('complete', function(data){
-    console.log(data);
-});
-
-quickscan.on('error', function(error){
-    console.log(error);
-});
-
-quickscan.startScan();
+}, 'SELECT * FROM macs');
 
 arpScanner(onResult);
 
 function onResult(err, data){
     if(err) throw err;
-    //console.log(data);
+
+    arp = data;
 }
 
+console.log(db);
+console.log(arp);
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
 });
